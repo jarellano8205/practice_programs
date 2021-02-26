12eba8c1ade1dc0b9b0d9d024e66260855d049f9
Johnny <johnathan.arellano@mines.sdsmt.edu>
2021-02-26 16:51:30 -0700

    refactored to make functions and variables be more self descriptive, used current tests to keep functionality

--- telemetry/telem.cpp
+++ telemetry/telem.cpp
@@ -3,31 +3,33 @@
 StopWatch::StopWatch()
 {}
 
-void StopWatch::start(std::string descr)
+void StopWatch::start(std::string description)
 {
-   timeLoc = std::chrono::system_clock::now();
-   taskDescription = descr;
+   startTime = std::chrono::system_clock::now();
+   taskDescription = description;
 }
 
 void StopWatch::stop()
 {
-   dur += std::chrono::system_clock::now() - timeLoc;
-   timeCompleted = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+   taskDuration += std::chrono::system_clock::now() - startTime;
+   
+   timeCompleted =
+   std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 }
 
 void StopWatch::reset()
 {
-   dur = std::chrono::duration<double>::zero();
+   taskDuration = std::chrono::duration<double>::zero();
 }
 
 double StopWatch::reportDuration()
 {
-   return double(dur.count());
+   return double(taskDuration.count());
 }
 void StopWatch::reportToConsole()
 {
    std::cout << "completed " << taskDescription << std::endl;
-   std::cout << std::fixed << dur.count() << "s\t\t" 
+   std::cout << std::fixed << taskDuration.count() << "s\t\t" 
       << std::ctime(&timeCompleted) << std::endl;
 }
 
@@ -48,16 +50,16 @@ void StopWatch::reportToFile(std::string fileName)
       std::cout << error << std::endl;
    }
    report << "completed " << taskDescription << std::endl;
-   report << std::fixed << dur.count() << "s\t\t"
+   report << std::fixed << taskDuration.count() << "s\t\t"
           << std::ctime(&timeCompleted) << std::endl;
    report.close();
 }
 
-std::stringstream StopWatch::reportSS()
+std::stringstream StopWatch::reportToSS()
 {
    std::stringstream ss;
    ss << "completed " << taskDescription << std::endl;
-   ss << std::fixed << dur.count() << "s\t\t" 
+   ss << std::fixed << taskDuration.count() << "s\t\t" 
       << std::ctime(&timeCompleted) << std::endl;
 
    return ss;
--- telemetry/telem.h
+++ telemetry/telem.h
@@ -2,27 +2,30 @@
 #define TELEM_H
 #include <chrono>
 #include <ctime>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <sstream>
+#include <string>
 
 class StopWatch
 {
-   std::chrono::system_clock::time_point timeLoc;
-   std::chrono::duration<double> dur = std::chrono::duration<double>::zero();
+   std::chrono::system_clock::time_point startTime;
+   
+   std::chrono::duration<double> taskDuration =
+   std::chrono::duration<double>::zero();
+   
    std::string taskDescription = "";
    std::time_t timeCompleted;
 public:
    StopWatch();
-   void start(std::string descr);
+   void start(std::string description);
    void stop();
    void reset(); 
    double reportDuration(); 
    void reportToConsole();
    void reportToFile(std::string fileName);
-   std::stringstream reportSS(); 
+   std::stringstream reportToSS(); 
 };
 
 #endif
